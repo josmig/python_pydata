1159644656a2edd059a1b0aad7038797abeee682
Miguel Pantigoso <miguelpantigoso@MacBook-Pro-de-Miguel.local>
2025-06-06 22:15:08 -0500

    iniciando el camino;

---
+++ modulo_1/type_python.py
@@ -0,0 +1,20 @@
+
+text = 'Hola, Python'
+text2 = "Hola mundo"
+
+result = text + text2
+print(result)
+
+
+message = 'Hola, Miguel'
+print(message)
+
+text_multi_line = """
+Esta es una 
+cadena de varias
+lineas
+en Python
+esto es muy util
+"""
+
+print(text_multi_line)

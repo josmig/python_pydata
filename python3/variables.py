b386013f8628953244c88240bc940711ab8e183a
Miguel Pantigoso <miguelpantigoso@MacBook-Pro-de-Miguel.local>
2025-11-23 22:17:18 -0500

    nuevo programacio

---
+++ python3/variables.py
@@ -0,0 +1,23 @@
+
+
+num1=5
+print(type(num1))
+
+num2=6.5
+print(type(num2))
+
+num3=7
+print(type(num3))
+
+suma = num1+num2+num3
+
+print(suma,type(suma))
+
+
+numer1=5
+numer2=3
+sumaa=numer1+numer2
+strSuma=str(sumaa)
+resultado = 'El resultado es ' +strSuma
+
+print(resultado,type(resultado))
